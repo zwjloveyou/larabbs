bde99334bfe1faf001901ec244b99e49ba9ddf17
zwj <2834577366@qq.com>
2022-07-01 10:35:25 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,4 @@ Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
