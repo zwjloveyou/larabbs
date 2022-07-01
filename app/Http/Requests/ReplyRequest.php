6e9348317f52aa84d77b53fd0b7773da984576f1
zwj <2834577366@qq.com>
2022-07-01 14:38:38 +0800

    用户可以发表评论

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 class ReplyRequest extends Request
 {
-    public function rules()
+/*    public function rules()
     {
         switch($this->method())
         {
@@ -30,12 +30,18 @@ class ReplyRequest extends Request
                 return [];
             }
         }
+    }*/
+    public function rules()
+    {
+        return [
+            'content' => 'required|min:2',
+        ];
     }
-
     public function messages()
     {
         return [
-            // Validation messages
+            'content.required' => '内容不能为空',
+            'content.min' => '内容必须至少两个字符',
         ];
     }
 }
