5cd6aa9dd3c34e853befe1c50e5ac8e4699f84b5
zwj <2834577366@qq.com>
2022-07-01 13:52:47 +0800

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->id == $model->user_id;
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
